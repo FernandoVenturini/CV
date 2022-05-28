9b5c6c1dbfd78c1f61a0e547324577b61738b7fc
Fernando Venturini <fernandoventurini9@hotmail.com>
2022-05-28 17:16:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/* demo */
+function entrar() {
+    document.getElementById("demo").innerHTML = "Ver Projeto";
+}
+
+function sair() {
+    document.getElementById("demo").innerHTML = "Link Projeto 01";
+}
+
+/* demo2 */
+function entrar() {
+    document.getElementById("demo2").innerHTML = "Ver Projeto";
+}
+
+function sair() {
+    document.getElementById("demo2").innerHTML = "Link Projeto 02";
+}
+
+/* demo3 */
+function entrar() {
+    document.getElementById("demo3").innerHTML = "Ver Projeto";
+}
+
+function sair() {
+    document.getElementById("demo3").innerHTML = "Link Projeto 03";
+}
+
+/* demo4 */
+function entrar() {
+    document.getElementById("demo4").innerHTML = "Ver Projeto";
+}
+
+function sair() {
+    document.getElementById("demo4").innerHTML = "Link Projeto 04";
+}
+
+/* demo5 */
+function entrar() {
+    document.getElementById("demo5").innerHTML = "Ver Projeto";
+}
+
+function sair() {
+    document.getElementById("demo5").innerHTML = "Link Projeto 05";
+}
